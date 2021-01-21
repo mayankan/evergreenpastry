70be47cb9e302debd40b988dbee9c8cf17f5d1c8
Mayank Anand <mayankan@gmail.com>
2021-01-21 13:53:17 +0530

    Images & Gallery

--- js/paging.js
+++ js/paging.js
@@ -1,34 +1,34 @@
-$('#pagination-demo').twbsPagination({
+$('#pagination').twbsPagination({
     totalPages: 4,
     // the current page that show on start
-    startPage: 1,
-  
+    startPage: 3,
+
     // maximum visible pages
     visiblePages: 4,
-  
+
     initiateStartPageClick: true,
-  
+
     // template for pagination links
     href: false,
-  
+
     // variable name in href template for page number
     hrefVariable: '{{number}}',
-  
+
     // Text labels
-    first: '<<',
+    first: '',
     prev: '<',
     next: '>',
-    last: '>>',
-  
+    last: '',
+
     // carousel-style pagination
     loop: false,
-  
+
     // callback function
-    onPageClick: function (event, page) {
-      $('.page-active').removeClass('page-active');
-      $('#page'+page).addClass('page-active');
+    onPageClick: function(event, page) {
+        $('.page-active').removeClass('page-active');
+        $('#page' + page).addClass('page-active');
     },
-  
+
     // pagination Classes
     paginationClass: 'pagination',
     nextClass: 'next',
@@ -38,5 +38,5 @@ $('#pagination-demo').twbsPagination({
     pageClass: 'page',
     activeClass: 'active',
     disabledClass: 'disabled'
-  
-  });
+
+});
